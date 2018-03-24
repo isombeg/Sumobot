4fb5869ddea724e65fd3ece6d7bc3f9ded6c981a
Guy-Jacques Isombe <isombeg@mcmaster.ca>
2018-03-24 14:46:18 -0400

    Add files via upload

---
+++ Motor_Test2.ino
@@ -0,0 +1,20 @@
+const int leftForward = 9;
+const int leftBackward = 10;
+const int rightForward = 11;
+const int rightBackward = 12;
+
+void setup() {
+  pinMode(leftForward , OUTPUT);
+  pinMode(leftBackward , OUTPUT);
+  pinMode(rightForward , OUTPUT);
+  pinMode(rightBackward , OUTPUT);
+
+}
+
+void loop(){
+  digitalWrite(leftForward , HIGH);
+  digitalWrite(leftBackward , LOW);
+  digitalWrite(rightForward , HIGH);
+  digitalWrite(rightBackward , LOW);
+  
+}
